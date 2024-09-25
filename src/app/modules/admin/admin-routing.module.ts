827ca4f76b677b50c4c04f6e0b221af78c7247a7
ShaileshA1-hexaware <shailesha1@hexaware.com>
2024-09-25 16:49:31 +0530

    Changes:Menubar

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
 
 const routes: Routes = [{
-  path:'',component:AdminDashboardComponent
+  path: '', component: AdminDashboardComponent, children: [
+    { path: 'home', component: HomeComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'about', component: AboutusComponent },
+    {path:'',redirectTo:'/admin/home',pathMatch:'full'}
+  ]
 }];
 
 @NgModule({
